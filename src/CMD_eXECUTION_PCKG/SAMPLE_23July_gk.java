8762452275a80b69ff7ffd818e46c9186c6b1bf5
GopalKhadeDac <khadegk@gmail.com>
2024-07-23 17:06:41 +0530

    gk tired

---
+++ src/CMD_eXECUTION_PCKG/SAMPLE_23July_gk.java
@@ -0,0 +1,9 @@
+package CMD_eXECUTION_PCKG;
+
+public class SAMPLE_23July_gk {
+	
+	public static void main(String[] args) {
+		System.out.println("code by gk");
+	}
+
+}
---
+++ src/cmd_pckg/sample_1_gk.java
@@ -0,0 +1,10 @@
+package cmd_pckg;
+
+public class sample_1_gk {
+
+	
+	public static void main(String[] args) {
+		
+		System.out.println("gk_1");
+	}
+}
