8e7fd7c2922b6fb0eaa6ffdcde9104c8abc7cb73
GopalKhadeDac <khadegk@gmail.com>
2024-07-23 17:53:56 +0530

    gk

---
+++ src/cod_gk.java
@@ -0,0 +1,6 @@
+
+public class cod_gk {
+	
+	
+
+}
---
+++ src/new_Package/codeByGk_1.java
@@ -0,0 +1,10 @@
+package new_Package;
+
+public class codeByGk_1 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("code by gk 1");
+	}
+
+}
